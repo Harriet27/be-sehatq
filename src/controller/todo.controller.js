4864dc70c1f7650852a39ff9609e550148803efb
aldrich.neil.bvt <aldrich.neil@bvarta.com>
2022-04-18 09:26:02 +0700

    fix encrypt helper function

--- src/controller/todo.controller.js
+++ src/controller/todo.controller.js
@@ -1,4 +1,5 @@
 const db = require("../config/database");
+const encrypt = require("../helper/encrypt");
 
 exports.get = async (req, res) => {
     const { rows } = await db.query(
@@ -8,5 +9,12 @@ exports.get = async (req, res) => {
         status: 201,
         data: rows,
         message: "GET successfully!",
+    });
+};
+
+exports.encrypt = async (req, res) => {
+    const { word } = req.body;
+    res.status(200).send({
+        encrypted_token: encrypt(word),
     });
 };
--- src/routes/todo.routes.js
+++ src/routes/todo.routes.js
@@ -2,5 +2,6 @@ const router = require('express-promise-router')();
 const todoController = require('../controller/todo.controller');
 
 router.get('/', todoController.get);
+router.post('/encrypt', todoController.encrypt);
 
 module.exports = router;
